cd1f4cee6ab79e4f0caa8553fff80913c525a620
gfernandez <gorka.fernandezg@alumni.mondragon.edu>
2024-05-16 11:00:31 +0200

    Header

--- murrikulum/src/main/java/app/backend/Main.java
+++ murrikulum/src/main/java/app/backend/Main.java
@@ -1,7 +1,15 @@
 package app.backend;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
+@ComponentScan(basePackages = {"app.backend", "app.frontend"})
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
     }
 }
