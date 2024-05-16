ce6bceb76b21c4ff84ea7ed7db2ead2054d71cae
gfernandez <gorka.fernandezg@alumni.mondragon.edu>
2024-05-16 11:36:25 +0200

    Home

--- murrikulum/src/main/java/app/backend/Main.java
+++ murrikulum/src/main/java/app/backend/Main.java
@@ -3,10 +3,8 @@ package app.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
 @ComponentScan(basePackages = {"app.backend", "app.frontend"})
 public class Main {
     public static void main(String[] args) {
