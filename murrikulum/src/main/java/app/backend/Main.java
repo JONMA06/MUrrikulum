30548897320daa64376f2cdeaa26a0f36823d5cb
gfernandez <gorka.fernandezg@alumni.mondragon.edu>
2024-05-16 12:20:21 +0200

    Fix thymeleaf structure

--- murrikulum/src/main/java/app/backend/Main.java
+++ murrikulum/src/main/java/app/backend/Main.java
@@ -2,10 +2,8 @@ package app.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"app.backend", "app.frontend"})
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
