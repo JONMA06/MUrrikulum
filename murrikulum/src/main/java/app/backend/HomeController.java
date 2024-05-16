c6ae69e994df29c2cbf7a59baf6aa7036d09461c
gfernandez <gorka.fernandezg@alumni.mondragon.edu>
2024-05-16 22:08:21 +0200

    Springboot functional

--- murrikulum/src/main/java/app/backend/HomeController.java
+++ murrikulum/src/main/java/app/backend/HomeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping(value = {"/", "/home"})
+    @GetMapping(value = {"/", "/etxea"})
     public String redirectHome() {
-        return "home"; 
+        return "home";
     }
 }
---
+++ murrikulum/src/main/java/app/backend/NavigationController.java
@@ -0,0 +1,18 @@
+package app.backend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NavigationController {
+
+    @GetMapping("/saioa_hasi")
+    public String saioaHasi() {
+        return "login"; 
+    }
+
+    @GetMapping("/izena_eman")
+    public String izenaEman() {
+        return "register"; 
+    }
+}
