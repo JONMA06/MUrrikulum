9a0b7801b788703c2922e67ea5eba7336af561e0
gfernandez <gorka.fernandezg@alumni.mondragon.edu>
2024-05-17 11:08:22 +0200

    NavigationController

--- murrikulum/src/main/java/app/backend/NavigationController.java
+++ murrikulum/src/main/java/app/backend/NavigationController.java
@@ -1,7 +1,13 @@
 package app.backend;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import app.backend.model.User;
+import app.backend.repository.UserRepository;
 
 @Controller
 public class NavigationController {
@@ -18,5 +24,18 @@ public class NavigationController {
     @GetMapping("/saioa_amaitu")
     public String saioaAmaitu() {
         return "home"; 
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String username, @RequestParam String password, Model model) {
+        User user = UserRepository.findByErabiltzaileaAndPasahitza(username, password);
+        if (user != null) {
+            // Usuario autenticado correctamente
+            return "redirect:/home";
+        } else {
+            // Credenciales inválidas, mostrar mensaje de error
+            model.addAttribute("error", true);
+            return "login";
+        }
     }
 }
