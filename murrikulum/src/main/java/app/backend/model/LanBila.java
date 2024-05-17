eec3b56b45f7b555cf31fe49a827945bbbd44a2b
gfernandez <gorka.fernandezg@alumni.mondragon.edu>
2024-05-17 10:58:54 +0200

    SQL 3

--- murrikulum/src/main/java/app/backend/model/LanBila.java
+++ murrikulum/src/main/java/app/backend/model/LanBila.java
@@ -1,9 +1,15 @@
 package app.backend.model;
 
-import javax.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "user")
+@Table(name = "lan_bila")
 public class LanBila {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
